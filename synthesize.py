efbb61aac162dfef2bf78bfd03beaffa660c2c52
Thea Aarrestad <thea.aarrestad@cern.ch>
2023-03-17 10:02:17 +0100

    adding latest model

--- synthesize.py
+++ synthesize.py
@@ -25,7 +25,6 @@ def print_dict(d, indent=0):
     else:
       print(':' + ' ' * (20 - len(key) - 2 * indent) + str(value))
 
-
 # Build floating point Keras model     
 def buildKeras(): 
   inputs = Input(shape=[98, ], name='input_features')
@@ -38,8 +37,6 @@ def buildKeras():
   model.summary()
 
   # Synthesise
-
-
   config = hls4ml.utils.config_from_keras_model(model, granularity='model',default_reuse_factor=1, default_precision='ap_fixed<16,6>')
   config['Model']['Strategy'] = 'Latency'
   print("-----------------------------------")
@@ -53,33 +50,31 @@ def buildKeras():
   cfg['XilinxPart'] = 'xczu9eg-ffvb1156-2-e'
   cfg['ClockPeriod'] = 10
   print_dict(cfg)
-  hls_model = hls4ml.converters.keras_to_hls(cfg)
-  # hls_model = hls4ml.converters.convert_from_keras_model(model,
-  #                                                        hls_config=config,
-  #                                                        output_dir='/mnt/data/thaarres/dnd/keras_model/hls4ml_prj',
-  #                                                        backend='Vivado',
-  #                                                        part='xczu9eg-ffvb1156-2-e')                                                   
+  hls_model = hls4ml.converters.keras_to_hls(cfg)                                            
   hls_model.compile()
   hls_model.build(csim=False, synth=True, vsynth=True)
 
 # Build quantized QKeras model    
 def buildQKeras():  
-  inputs = Input(shape=[98, ], name='qinput_features')
-  x = QDense(20, kernel_quantizer=quantized_bits(4,0,alpha=1), bias_quantizer=quantized_bits(4,0,alpha=1), name='fc1')(inputs)
-  x = QActivation(activation=quantized_relu(4), name='relu1')(x)
-  x = QDense(1, kernel_quantizer=quantized_bits(4,0,alpha=1), bias_quantizer=quantized_bits(4,0,alpha=1), name='output')(x)
-  # x = Activation(activation="sigmoid", name='output_act')(x)
-  x = QActivation("quantized_relu(bits=16,integer=0,use_sigmoid=1)", name='output_act')(x)
-  model = Model(inputs, x)  
-  model.load_weights('4bitMLPmodel.h5')
-  optimizer = Adam(learning_rate=0.001)
-  model.compile(optimizer, loss='mean_squared_error', metrics=['accuracy'])
-  model.summary()
+  inputs = Input(shape=[98, ], name='inputlayer_')
+  x = QActivation("quantized_bits(4,alpha=1)", name="qact0")(inputs)
+  x = QDense(10, input_shape=(98, ), 
+             kernel_quantizer=quantized_bits(4,alpha=1),
+             bias_quantizer=quantized_bits(4,alpha=1),
+             name="fc1")(x)  
+  x = QActivation("quantized_relu(4)", name="relu1")(x)
+  x = QDense(1,
+            kernel_quantizer=quantized_bits(4,alpha=1),
+            bias_quantizer=quantized_bits(4,alpha=1),
+            name="fc2")(x)
+  x = Activation('sigmoid',name='output')(x)
+  model = Model(inputs, x)
+  model.load_weights('0316_soft_4bit_alpha1_sigmoid.h5') #Loading weights of pre-trained model
 
+        
   # Synthesise
   config = hls4ml.utils.config_from_keras_model(model, granularity='name',default_reuse_factor=1)
   config['Model']['Strategy'] = 'Latency'
-  config['LayerName']['fc1']['Strategy'] = 'Resource'
   print("-----------------------------------")
   print("Configuration")
   print_dict(config)
@@ -91,16 +86,9 @@ def buildQKeras():
   cfg['Part'] = 'xc7z100-ffg900-2'
   cfg['ClockPeriod'] = 10
   print_dict(cfg)
-  hls_model = hls4ml.converters.keras_to_hls(cfg)
-  # hls_model = hls4ml.converters.convert_from_keras_model(model,
-  #                                                        hls_config=config,
-  #                                                        output_dir='/mnt/data/thaarres/dnd/qkeras_model/hls4ml_prj',
-  #                                                        backend='Vivado',
-  #                                                        part='xc7z100-ffg900-2')                                                       
+  hls_model = hls4ml.converters.keras_to_hls(cfg)                                                 
   hls_model.compile()
-  # hls_model.build(reset=False, csim=False, synth=True, cosim=True, validation=True, export=True, vsynth=True)
   hls_model.build(csim=False, synth=True, vsynth=True)
-
 
 # Initiate the parser
 parser = argparse.ArgumentParser()
